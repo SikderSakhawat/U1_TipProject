2c36560e3bf744cb1b4bd1d5dc9d6cc171e41cfe
BT_4N2_34 <ssakhawat4381@bths.edu>
2022-09-21 12:27:08 -0400

    Started on my "receipt" after adding in all the necessary variables needed (for now).

--- src/Reciept.java
+++ src/Reciept.java
@@ -1,6 +1,9 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.*;
 public class Reciept {
     public static void main(String[] args) {
+        DecimalFormat currency = new DecimalFormat("###0.00");
         Scanner s = new Scanner(System.in);
         System.out.println("How much is your bill?");
         double totalCash = s.nextDouble();
@@ -8,6 +11,10 @@ public class Reciept {
         int numPeople = s.nextInt();
         System.out.println("How much percent in tip will you be giving (please choose a number from 0 to 100) ");
         double tipAmount = s.nextDouble();
-        
+        double billPerPerson =totalCash/numPeople;
+        double tipPerPerson = billPerPerson*tipAmount;
+        double totalPerPerson = tipPerPerson + billPerPerson;
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
     }
 }
