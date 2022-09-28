36971ef5b132dd73a9fc2858a83eecbe5a154eeb
SikderSakhawat <114007137+SikderSakhawat@users.noreply.github.com>
2022-09-27 23:51:31 -0400

    Finished up tip, allowed for the decimal to be rounded to the hundredths place. Also put comments on sections of my code to explain each portion and what they do.

--- src/Reciept.java
+++ src/Reciept.java
@@ -1,20 +1,38 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
-import java.util.*;
 public class Reciept {
     public static void main(String[] args) {
-        DecimalFormat currency = new DecimalFormat("###0.00");
         Scanner s = new Scanner(System.in);
         System.out.println("How much is your bill?");
-        double totalCash = s.nextDouble();
+        double totalCash = s.nextDouble(); // shows the current bill amount, without tip
         System.out.println("How many people are in your party?");
-        int numPeople = s.nextInt();
+        int numPeople = s.nextInt(); // gives the number of people who will be paying for the bill
         System.out.println("How much percent in tip will you be giving (please choose a number from 0 to 100) ");
-        double tipAmount = s.nextDouble();
-        double billPerPerson =totalCash/numPeople;
-        double tipPerPerson = billPerPerson*tipAmount;
+        double tipAmount = s.nextDouble(); // gives the % of tip needed
+        double billPerPerson = totalCash / numPeople;
+        // shows the amount of cash each person will pay, not including tip
+        double tipPerPerson = billPerPerson * (tipAmount/100);
+        // gives the amount of tip each person will pay
         double totalPerPerson = tipPerPerson + billPerPerson;
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        // gives the total amount each person has to pay with tip
+        double billWithTip = totalPerPerson * numPeople;
+        // gives the total amount needed to pay for the bill, with tip
+        double totalTip = billWithTip - totalCash; // gives the total bill with tip
+        String design = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
+        // OUTPUT BELOW
+        System.out.println(design);
+        System.out.printf("Total Cash: $"+ "%.2f" + "\n", totalCash);
+        System.out.printf("Your bill with tip is: $" + "%.2f" + "\n", billWithTip);
+        System.out.printf("Bill per person, without tip: $" + "%.2f" + "\n",billPerPerson);
+        System.out.printf("Total tip: $" + "%.2f" + "\n", totalTip);
+        // everything above outputs anything pertaining to the total bill or tip
+        System.out.println();
+        System.out.println("Number of people: " + numPeople); // tells you number of people
+        System.out.println();
+        System.out.println("Percentage of tip given: " + tipAmount + "%");
+        System.out.printf("Amount of tip per person: $" + "%.2f" + "\n", tipPerPerson);
+        System.out.printf("Total bill per person: $" + "%.2f" + "\n", totalPerPerson);
+        // everything above outputs anything pertaining to the cost and tip per person
+        System.out.println(design);
     }
 }
